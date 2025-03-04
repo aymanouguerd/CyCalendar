af9b48fee8c7a3038ef1532228c04b2d7f112d10
Adam <NayJi7@github.com>
2025-03-04 08:56:51 +0100

    Mise à jour de webdriver-manager vers la version 4.0.1 et suppression d'une importation inutile dans auth.py

--- src/auth.py
+++ src/auth.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from webdriver_manager.core.utils import ChromeType
 
 # Import Display uniquement sous Linux
 if platform.system() != "Windows":
